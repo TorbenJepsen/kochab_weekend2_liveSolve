59ffee5a909f2a76c4a5b1508e903fdae3429beb
devjanaprime <dev@primeacademy.io>
2018-04-23 10:24:41 -0500

    after second lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,25 +1,80 @@
 console.log( 'js' )
 
+let currentType = '';
+
+let historyNow = () =>{
+    console.log( 'in historyNow' );
+    // GET call to /history
+    $.ajax({
+        method: 'GET',
+        url: '/history'
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+        // display on DOM
+        let el = $( '#historyOut' );
+        el.empty();
+        // loop through the array for ul elements
+        for( let i=0; i<response.history.length; i++){
+            el.append( '<li>' + response.history[i].x + ' ' +
+             response.history[i].type + ' ' + response.history[i].y + '</li>' );
+        } // end for
+    }) // end ajax
+} // end historyNow
+
+let answerMeThis = () =>{
+    console.log( 'in answerMeThis' ); 
+    $.ajax({
+        method: 'GET',
+        url: '/answer'
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+        // target #answerOut, clear, and append answer
+        let el = $( '#answerOut' );
+        el.empty();
+        el.append( 'Answer: ' + response.answer );
+    })
+} // end answerMeThis
+
 let doMathNow = () => {
     console.log( 'in doMathNow' );
     // target elements with ids of xIn and yIn and get their val
     // create object to send
     let objectToSend = {
         x: $( '#xIn ').val(),
-        y: $( '#yIn ').val()
+        y: $( '#yIn ').val(),
+        type: currentType
     } // end objectToSend
-    console.log( objectToSend );
+    console.log( 'sending to server:', objectToSend );
+    $.ajax({
+        method: 'POST',
+        url: '/doMath',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+        answerMeThis();
+        historyNow();
+    }) // end ajax
 } // end doMathNow
 
-let setOperator = () => {
-    console.log( $( this ).data( 'operatorType' ) );
-}
+let clearAll = () =>{
+    console.log( 'in clearAll' );
+    $( '#xIn ').val( '' );
+    $( '#yIn ').val( '' );
+    currentType = '';
+} // end clearAll
+
+function setOperator(){
+    console.log( 'in setOperator:', $( this ).text() );
+    currentType = $( this ).text();
+} // end setOperator
 
 let readyNow = () => {
     console.log( 'JQ' );
     // click handler for element with id of doMathButton
     $( '#doMathButton' ).on( 'click', doMathNow );
-    $( '.operatorButton').on( 'click', setOperator );
+    $( '#goAwayButton' ).on( 'click', clearAll );
+    $( '.operatorTypeButtonThing' ).on( 'click', setOperator );
+    historyNow();
 } // end doc ready
 
 $( document ).ready( readyNow );
--- server/server.js
+++ server/server.js
@@ -9,8 +9,36 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
 const port = 23456;
+let currentAnswer = 0;
+let history = [];
 
 // spin up server
 app.listen( port, function(){
     console.log( 'server up on:', port );
 }); // end server up
+
+app.get( '/answer', function( req, res ){
+    res.send( { answer: currentAnswer } );
+});
+
+app.get( '/history', function( req, res ){
+    res.send( { history: history } );
+});
+
+app.post( '/doMath', function( req, res ){
+    console.log( 'in doMath POST:', req.body );
+    history.unshift( req.body );
+    if( req.body.type === '-' ){
+        currentAnswer = Number( req.body.x ) - Number( req.body.y ); 
+    }
+    else if( req.body.type === '*' ){
+        currentAnswer = Number( req.body.x ) * Number( req.body.y ); 
+    }
+    else if( req.body.type === '/' ){
+        currentAnswer = Number( req.body.x ) / Number( req.body.y ); 
+    }
+    else{
+        currentAnswer = Number( req.body.x ) + Number( req.body.y ); 
+    }
+    res.sendStatus( 200 );
+});
